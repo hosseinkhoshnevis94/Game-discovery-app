9e57196e94af37184649e6cf8034b8441454256c
Hossein <hossein.khoshnevis77@gmail.com>
2023-09-19 16:18:15 +0200

    create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import { Button } from '@chakra-ui/react'
-
+import { Grid, GridItem,Show } from '@chakra-ui/react'
 const App = () => {
   return (
     <>
-<Button colorScheme='blue'>Button</Button>
+    <Grid
+  templateAreas={{
+    base:`"header header"
+    "main main"`,
+    lg:`"header header"
+    "nav main"`
+  }}
+  gridTemplateRows={'50px 1fr 30px'}
+  gridTemplateColumns={'150px 1fr'}
+  h='200px'
+  gap='1'
+  color='blackAlpha.700'
+  fontWeight='bold'
+>
+  <GridItem pl='2' bg='orange.300' area={'header'}>
+    Header
+  </GridItem>
+  <Show above='lg'>
+  <GridItem pl='2' bg='pink.300' area={'nav'}>
+    Nav
+  </GridItem>
+  </Show>
+  <GridItem pl='2' bg='green.300' area={'main'}>
+    Main
+  </GridItem>
+
+</Grid>
     
     </>
   )
